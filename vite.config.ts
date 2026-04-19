ff8fcf7e74652cd1886f5a188055f1939673a250
CAPRINA-OS <captainfahd5@gmail.com>
2026-04-19 22:03:02 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/",
   plugins: [
     react(),
-    tailwindcss(),
   ],
   resolve: {
     alias: {
@@ -17,9 +14,5 @@ export default defineConfig({
   build: {
     outDir: "dist",
     emptyOutDir: true,
-  },
-  server: {
-    port: 3000,
-    host: "0.0.0.0",
   },
 });
